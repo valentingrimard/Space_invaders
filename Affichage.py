457464547beacd350e0455a1e2edb4cfee77ac11
valentingrimard <valentin.grimard@cpe.fr>
2022-12-12 15:21:06 +0100

    display ship

--- Affichage.py
+++ Affichage.py
@@ -7,8 +7,8 @@
 
 #####
 
-import tkinter as tk
-
+from tkinter import Tk, Label, Button, StringVar, Frame, Canvas
+from Ship import cShip
 
 class Affichage():
     """
@@ -21,42 +21,47 @@ class Affichage():
         minsize et maxsize : En donnant la même valeur à minsize et maxsize on a une fenêtre qui ne peut pas être agrandie ni réduite à la main
         config : Permet de configurer la fenêtre, notamment la couleur du fond
         """
-        self.window = tk.Tk()
+        self.window = Tk()
         self.window.title("Space Invaders")
         self.window.geometry("800x800")
         self.window.minsize(800, 800)
         self.window.maxsize(800, 800)
         self.window.config(background = "#FFFFFF")
-        self.create_menu_bar()
+        #self.create_menu_bar()
 
         #Crée la fenêtre initiale
-        self.frame = tk.Frame(self.window, bg = "#FFFFFF")
+        self.frame = Frame(self.window, bg = "#FFFFFF")
         self.frame.pack(expand = True)
 
         #Change le titre de la fenêtre
-        self.label = tk.Label(self.frame, text = "Space Invaders", font = ("Helvetica", 40), bg = "#FFFFFF")
+        self.label = Label(self.frame, text = "Space Invaders", font = ("Helvetica", 40), bg = "#FFFFFF")
         self.label.pack()
 
         #Crée le bouton JOUER
-        self.button = tk.Button(self.frame, text = "Jouer", font=("Helvetica", 20), bg = "#FFFFFF", command = self.play)
+        self.button = Button(self.frame, text = "Jouer", font=("Helvetica", 20), bg = "#FFFFFF", command = self.play)
         self.button.pack(pady=20)
 
         #Crée le bouton QUITTER
-        self.button = tk.Button(self.frame, text = "Quitter", font=("Helvetica", 20), bg = "#FFFFFF", command = self.window.quit)
+        self.button = Button(self.frame, text = "Quitter", font=("Helvetica", 20), bg = "#FFFFFF", command = self.window.quit)
+        self.button.pack(pady=20)
+
+        #Crée le bouton A PROPOS 
+        self.button = Button(self.frame, text = "A Propos", font=("Helvetica", 20), bg = "#FFFFFF", command = self.about)
         self.button.pack(pady=20)
 
     #Création du menu
-    def create_menu_bar(self):
-        menu_bar = tk.Menu(self)
-        menu_game = tk.Menu(menu_bar, tearoff = 0)
-        menu_game.add_command(label = "Rejouer", command = self.play)
-        menu_game.add_command(label = "Quitter", command = self.window.quit)
-        menu_game.add_command(label = "A propos", command = self.about)
+    #def create_menu_bar(self):
+    #    menu_bar = Menu(self)
+    #    menu_game = Menu(menu_bar, tearoff = 0)
+    #    menu_game.add_command(label = "Rejouer", command = self.play)
+    #    menu_game.add_command(label = "Quitter", command = self.window.quit)
+    #    menu_game.add_command(label = "A propos", command = self.about)
 
     #Création du "A propos"
     def about(self):
-        self.frame = tk.Frame(self.window, text="Jeu du Space Invaders \n Made by GRIMARD Valentin & SCHERDING Romain" , bg = "#FFFFFF")
-        self.frame.pack()
+        self.button.destroy()
+        self.button = Button(self.frame, text = "Jeu du Space Invaders \n Made by GRIMARD Valentin & SCHERDING Romain", font=("Helvetica", 20), bg = "#FFFFFF", command = self.button.destroy)
+        self.button.pack()
 
     def play(self):
         """
@@ -64,17 +69,17 @@ class Affichage():
         """
         #Détruit les fenêtres existantes et crée la fenêtre de jeu
         self.frame.destroy()
-        self.frame = tk.Frame(self.window, bg = "#FFFFFF")
+        self.frame = Frame(self.window, bg = "#FFFFFF")
         self.frame.pack(expand=True)
 
         #Crée la fenêtre dynamique dans laquelle on a le canvas (space invader)
-        self.dynamicFrame = tk.Frame(self.frame, bg = "#FFFFFF")
+        self.dynamicFrame = Frame(self.frame, bg = "#FFFFFF")
         self.create_dynamic_canvas()
         self.dynamicFrame.pack(expand = "YES", fill = "both")
 
         #Crée la zone de texte indiquant le score actuel
-        self.score = tk.StringVar()
-        self.labelscore = tk.Label(self.dynamicFrame, textvariable = self.score, font = ("Helvetica", 10), bg = "#FFFFFF")
+        self.score = StringVar()
+        self.labelscore = Label(self.dynamicFrame, textvariable = self.score, font = ("Helvetica", 10), bg = "#FFFFFF")
         self.labelscore.pack()
 
     def create_dynamic_canvas(self):
@@ -82,16 +87,32 @@ class Affichage():
         Fonction qui crée le canvas, dans lequel on va retrouver le space invader
         """
         #Crée la fenêtre canvas de taille 800x600
-        self.canvas = tk.Canvas(self.dynamicFrame, width = 800, height = 600, bg = "#C8C8C8")
+        self.canvas = Canvas(self.dynamicFrame, width = 800, height = 600, bg = "#C8C8C8")
         self.canvas.pack()
 
         #Crée le bouton JOUER
-        self.button = tk.Button(self.frame, text = "Jouer", font = ("Helvetica", 20), bg = "#FFFFFF", command = self.play)
+        self.button = Button(self.frame, text = "Jouer", font = ("Helvetica", 20), bg = "#FFFFFF", command = self.play)
         self.button.pack(side = 'top' , padx = 20)
 
         #Crée le bouton QUITTER
-        self.button = tk.Button(self.frame, text = "Quitter", font = ("Helvetica", 20), bg = "#FFFFFF", command = self.window.quit)
+        self.button = Button(self.frame, text = "Quitter", font = ("Helvetica", 20), bg = "#FFFFFF", command = self.window.quit)
         self.button.pack(side = 'top' , padx = 20)
+        
+        #Affiche le vaisseau
+        self.displayShip()
+
+    def displayShip(self):
+        """
+        Création du vaisseau
+        """
+        self.canvas = Canvas(self.dynamicFrame, width = 800, height = 600, bg = "#FFFFFF")
+        return self.canvas.create_oval(
+            cShip.getShipX(self)-cShip.getShipR(self),
+            cShip.getShipY(self)-cShip.getShipR(self),
+            cShip.getShipX(self)+cShip.getShipR(self),
+            cShip.getShipY(self)+cShip.getShipR(self),
+            outline='black',fill='white')
+
 
 app = Affichage()
 app.window.mainloop()
